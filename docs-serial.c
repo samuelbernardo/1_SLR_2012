8260dee055580cd7914dda47ebefc4a47d7a6cae
Samuel de Sousa Nascimento Bernardo <ist146425@borg.rnl.ist.utl.pt>
2012-10-16 16:41:24 +0100

    Falta testar o parse do ficheiro de input

--- docs-serial.c
+++ docs-serial.c
@@ -25,21 +25,23 @@ typedef struct document {
 unsigned num_cabinets;
 unsigned num_documents;
 unsigned num_subjects;
-document_t * documents;
 
 /* Parses the input (.in) file and creates all data according to its contents */
 void load_data(FILE *in)
 {
-	fscanf(in, "%d\n", &num_cabinets);
-	fscanf(in, "%d\n", &num_documents);
-	fscanf(in, "%d\n", &num_subjects);
-
+	document_t *documents;
 	int id_temp = 0;
 	int i,j;
 	char line[BUFFER_SIZE];	
 	char token;
 
-	while(!feof(in))
+	fscanf(in, "%d\n", &num_cabinets);
+	fscanf(in, "%d\n", &num_documents);
+	fscanf(in, "%d\n", &num_subjects);
+
+	documents = (document_t *)malloc(sizeof(document_t)*num_documents);
+
+	for(!feof(in))
 	{
 		if (fgets(line, BUFFER_SIZE, in) == NULL) {
     		printf("[fgets] Error reading line 1\n");
@@ -48,13 +50,17 @@ void load_data(FILE *in)
 		//initialize memory
 		//get document identifier
 		token = strtok(line, DELIMS);
-		id_temp = atoi(token);
-				
+		id_temp = strtol(token,NULL);
+		document = documents[id_temp];
+		document-> id = id_temp;
+		document->cabinet = id_temp%num_cabinets;
+		
+		document->scores = (double*)malloc(sizeof(double)*num_subjects);
 		//get subjects and add them to double vector
 		for(i = 0; i < num_subjects; i++)
 		{
-			
-			
+			token = strtok(NULL, DELIMS);
+			document->scores[i] = strtod(token,NULL);
 		}
 	}
 }
