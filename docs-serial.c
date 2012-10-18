044615c04e4a4898001055b19d323d3bfb3172b9
root <root@debian.(none)>
2012-10-18 17:28:25 +0100

    Almost complete version of serial version, main cycle is complete in it's main form, there are bus running other examples other then ex5-1d.in

--- docs-serial.c
+++ docs-serial.c
@@ -24,7 +24,8 @@ typedef struct document {
 	double *scores;
 } Document;
 
-Document* newDocument(int id, int cabinet, unsigned int num_subjects) {
+Document* newDocument(int id, int cabinet, unsigned int num_subjects) 
+{
 	Document *doc = (Document*) malloc(sizeof(Document));
 	doc->id = id;
 	doc->cabinet = cabinet;
@@ -32,14 +33,74 @@ Document* newDocument(int id, int cabinet, unsigned int num_subjects) {
 	return doc;
 }
 
-void freeDocument(Document *doc) {
+void freeDocument(Document *doc) 
+{
 	free(doc->scores);
 	free(doc);
 }
 
-void document_setScore(Document *doc, double score, unsigned int pos) {
+void document_setScore(Document *doc, double score, unsigned int pos) 
+{
 	doc->scores[pos] = score;
 }
+
+/* --- */
+
+/* Cabinet class */
+/*typedef struct {
+	Document document;
+	Document *next;
+} Element;
+
+typedef struct {
+	int id;
+	double *scores;
+	int size;
+	Element *list;
+}
+
+Cabinet *newCabinet(int id, unsigned int num_subjects)
+{
+	Cabinet *cab = (Cabinet *)malloc(sizeof(Cabinet));
+	cab->id = id;
+	cab->scores = (double *)malloc(sizeof(double)*num_subjects);
+	cab->size = 0;
+	cab->first = NULL;
+	return cab;
+}
+
+void freeCabinet(Cabinet *cab)
+{
+	int i;
+	Element *current = cab->first;
+	Element *temp;
+	free(cab->scores);
+	for(i = 0; i < cab->size; i++)
+	{
+		temp = current;
+		current = temp->next;
+		free(temp);
+	}
+	free(cab);
+}
+
+void addDocument(Cabinet * cab, Document *doc)
+{
+	Element *next = cab->first;
+	cab->first = doc;
+	cab->first->next=next;
+	cab->size++;
+}
+
+void removeDocument(Cabinet * cab, Document *doc)
+{
+	Element *current;
+	if(cab->first == NULL)
+	{
+		return;
+	}
+	
+}*/
 
 /* --- */
 
@@ -48,10 +109,12 @@ typedef struct data {
 	unsigned int num_cabinets;
 	unsigned int num_documents;
 	unsigned int num_subjects;
+	//Document ** cabinets;
 	Document **documents;
 } Data;
 
-Data *newData(unsigned int num_cabinets, unsigned int num_documents, unsigned int num_subjects) {
+Data *newData(unsigned int num_cabinets, unsigned int num_documents, unsigned int num_subjects) 
+{
 	Data *data = (Data*) malloc(sizeof(Data));
 	data->num_cabinets = num_cabinets;
 	data->num_documents = num_documents;
@@ -60,7 +123,8 @@ Data *newData(unsigned int num_cabinets, unsigned int num_documents, unsigned in
 	return data;
 }
 
-void freeData(Data *data) {
+void freeData(Data *data) 
+{
 	unsigned int i;
 	for(i = 0; i < data->num_documents; i++) {
 		freeDocument(data->documents[i]);
@@ -69,12 +133,14 @@ void freeData(Data *data) {
 	free(data);
 }
 
-void data_setDocument(Data *data, Document *doc, unsigned int pos) {
+void data_setDocument(Data *data, Document *doc, unsigned int pos) 
+{
 	data->documents[pos] = doc;
 }
 
 
-void data_printDocuments(Data *data) {
+void data_printDocuments(Data *data) 
+{
 	unsigned int i;
 	for(i = 0; i < data->num_documents; i++) {
 		printf("%u %u\n", data->documents[i]->id, data->documents[i]->cabinet);
@@ -104,7 +170,6 @@ Data *load_data(FILE *in) {
 	fscanf(in, "%u\n", &num_documents);
 	fscanf(in, "%u\n", &num_subjects);
 	data = newData(num_cabinets, num_documents, num_subjects);
-	
 	while(fgets(line, BUFFER_SIZE, in) != NULL) {
 		/*get document identifier*/
 		token = strtok(line, DELIMS);
@@ -121,14 +186,17 @@ Data *load_data(FILE *in) {
 	return data;
 }
 
-int main (int argc, char **argv) {
+int main (int argc, char **argv) 
+{
 	FILE *in;
 	Data *data; 
 	double elapsed_time;
 	int changed_flag = 1;
 	double **cabinets;
+	int *cabinet_sizes;
 	int i = 0;
 	int j = 0;
+	int k = 0;
 	
 	if(argc != 2)
 	{
@@ -142,11 +210,104 @@ int main (int argc, char **argv) {
 	
 	data = load_data(in);
 	fclose(in);
+	//debugging
+	//printf("documents pre-processing\n");
+	//data_printDocuments(data);
+
+	//create cabinets
+	cabinets = (double **)malloc(sizeof(double*)*data->num_cabinets);
+	for(i = 0; i < data->num_cabinets; i++)
+	{
+		cabinets[i] = (double *)calloc(data->num_subjects, sizeof(double));
+	}
+	cabinet_sizes = (int *)calloc(data->num_subjects, sizeof(int));
+	
+	//debbuging
+	/*printf("pre-averages: \n");
+	for(i = 0; i < data->num_cabinets; i++)
+	{
+		printf("cabinet %d: ", i);
+		for(j = 0; j < data->num_subjects; j++)
+		{
+			printf("%f ",cabinets[i][j]);
+		}
+		printf("\n");
+	}*/
+	
+	//main cycle stops when a document has not changed cabinets
+	while (changed_flag)
+	{
+		changed_flag = 0;
+		//1st step: calculate coordinates for the cabinets
+		//using the averages of their documents
+		//for each document
+		for(i = 0; i < data->num_documents; i++)
+		{
+			//for each subject
+			for(j = 0; j < data->num_subjects; j++)
+			{
+				Document *doc = data->documents[i];
+				//sum the score to the cabinet
+				cabinets[doc->cabinet][j] += doc->scores[j];
+				cabinet_sizes[doc->cabinet]++;
+			}
+		}
+		for(i = 0; i < data->num_cabinets; i++)
+		{
+			for(j = 0; j < data->num_subjects; j++)
+			{
+				//this step completes the calculation of the average
+				cabinets[i][j] = cabinets[i][j]/cabinet_sizes[i];
+			}
+		}
+		
+		//now that the cabinets have their scores calculated
+		//we calculate the distances
+		//from document to cabinet
+		//d = sum(a -b)^2 for each subject
+		for(i = 0; i < data->num_documents ;i++)
+		{
+			double current_sum = DBL_MAX;
+			double sum = 0;
+			Document *doc = data->documents[i];
+			int target_cabinet = doc->cabinet;
+			int current_cabinet = doc->cabinet;
+			for(j = 0; j < data->num_cabinets; j++)
+			{
+				sum = 0;
+				//we sum all the scores for a specific subject
+				for(k = 0; k < data->num_subjects; k++)
+				{
+					sum += pow(cabinets[j][k]-doc->scores[k],2);
+					//printf("doc[%d],cab[%d],sub[%d] : %f\n",i,j,k,sum);
+				}
+				//we check if the sum is smaller then the current_sum
+				
+				if(sum < current_sum)
+				{
+					current_sum = sum;
+					current_cabinet = j;
+				}
+			}
+			if(current_cabinet != target_cabinet)
+			{
+				doc->cabinet = current_cabinet;
+				changed_flag = 1;
+			}
+		}
+		//prepare next iteration
+		for(i = 0; i < data->num_cabinets; i++)
+		{
+			//for each subject
+			for(j = 0; j < data->num_subjects; j++)
+			{
+				cabinets[i][j] = 0;
+				cabinet_sizes[i] = 0;
+			}
+		}
+	}
+	//printf("documents post-processing\n");
 	data_printDocuments(data);
-	
-	
-	
-	
 	freeData(data);
 	return 0;
 }
